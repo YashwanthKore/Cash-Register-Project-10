3e56847e7edc303c470a1171585f77aa2a4a66f1
YashwanthKore <yashwanthkore97@gmail.com>
2021-09-17 15:26:17 +0530

    Cash register Project
    Mark 10 of Neogcamp

---
+++ script.js
@@ -0,0 +1,52 @@
+var billAmount = document.querySelector("#bill-amount");
+var givenAmount = document.querySelector("#given-amount");
+var btnCheck = document.querySelector("#btn-check");
+var noOfNotes = document.querySelectorAll(".no-of-notes");
+var errorMessageDiv = document.querySelector("#error-message-div");
+
+var notesArr = [2000, 500, 100, 20, 10, 5, 1]
+
+function showErrorMessage(errorMessage) {
+    errorMessageDiv.innerHTML = errorMessage;
+    // console.log(errorMessage)
+}
+function hideErrorMessage() {
+    errorMessageDiv.innerHTML = ""
+}
+
+function vadiladateAmounts() {
+
+    if (billAmount.value >= 0) {
+
+        if (givenAmount.value > billAmount.value) {
+            var amountToReturn = givenAmount.value - billAmount.value
+            //    console.log(amountToReturn)
+            hideErrorMessage()
+            calculateChange(amountToReturn)
+
+
+        } else {
+            var errorMessage = "*Please enter valid amount: Given amount should be greater than Bill amount"
+            showErrorMessage(errorMessage)
+        }
+
+    } else {
+        var errorMessage = "*Enter valid Bill Amount"
+        showErrorMessage(errorMessage)
+    }
+
+    function calculateChange(amountToReturn) {
+
+        for (var i = 0; i < notesArr.length; i++) {
+
+            var calculateNumberOfNotes = Math.trunc(amountToReturn / notesArr[i])
+            amountToReturn = amountToReturn % notesArr[i]
+            noOfNotes[i].innerHTML = calculateNumberOfNotes
+        }
+
+    }
+
+
+
+}
+btnCheck.addEventListener("click", vadiladateAmounts)
